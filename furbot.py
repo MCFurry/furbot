2a526640716dfedb1f12a134b293eefbda550221
Ferry Schoenmakers <f.b.f.schoenmakers@gmail.com>
2023-03-29 22:15:38 +0200

    Initial logic!

--- furbot.py
+++ furbot.py
@@ -1,37 +1,59 @@
-import random
-import numpy as np
-from ..bot_control import Move
-
-class FurBot:
-
-    def __init__(self):
-        self.target = None
-
-    def get_name(self):
-        return "FurBot"
-
-    def get_contributor(self):
-        return "Ferdinand Schumacher"
-
-    def determine_next_move(self, grid, enemies, game_info):
-        # Chooses a random target location, and moves there.
-        # Once it's there, choose a new location.
-
-        # Create a target in storage if doesn't exist
-        if  self.target is None:
-            self.target = np.zeros_like(self.position)
-
-        # If reached the target find a new target
-        if np.array_equal(self.position, self.target):
-            self.target[0] = random.randint(0, grid.shape[0] - 1)
-            self.target[1] = random.randint(0, grid.shape[1] - 1)
-
-        # Move in direction of target
-        if self.target[0] > self.position[0]:
-            return Move.RIGHT
-        elif self.target[0] < self.position[0]:
-            return Move.LEFT
-        elif self.target[1] > self.position[1]:
-            return Move.UP
-        else:
-            return Move.DOWN
+import random #line:1
+import numpy as np #line:2
+from ..bot_control import Move #line:3
+from copy import copy #line:4
+class FurBot :#line:6
+    def __init__ (OOO000000O0O000OO ):#line:8
+        OOO000000O0O000OO .target =None #line:9
+        OOO000000O0O000OO .grid_size =None #line:10
+        OOO000000O0O000OO .cross =[Move .UP ,Move .LEFT ,Move .DOWN ,Move .RIGHT ]#line:11
+    def get_name (OOO00O0O0O0O0000O ):#line:13
+        return "FurBot"#line:14
+    def get_contributor (OOOOOOO000OOO000O ):#line:16
+        return "Ferdinand Schumacher"#line:17
+    def clamp (O00O0OOO00O0O0O0O ,O0000OO0OOO0OOOOO ,O0O000OOOO0O0OO0O ,OO0OO0O0O0O000OOO ):#line:19
+        return max (O0O000OOOO0O0OO0O ,min (O0000OO0OOO0OOOOO ,OO0OO0O0O0O000OOO ))#line:20
+    def get_cell (OO0O000O0O00OOO0O ,OO0000OO000O0O00O ,O00OO0O0OO00OOOOO ):#line:22
+        if O00OO0O0OO00OOOOO is Move .UP :#line:23
+            return [OO0000OO000O0O00O [0 ],OO0000OO000O0O00O [1 ]+1 ]#line:24
+        elif O00OO0O0OO00OOOOO is Move .LEFT :#line:25
+            return [OO0000OO000O0O00O [0 ]-1 ,OO0000OO000O0O00O [1 ]]#line:26
+        elif O00OO0O0OO00OOOOO is Move .DOWN :#line:27
+            return [OO0000OO000O0O00O [0 ],OO0000OO000O0O00O [1 ]-1 ]#line:28
+        else :#line:29
+            return [OO0000OO000O0O00O [0 ]+1 ,OO0000OO000O0O00O [1 ]]#line:30
+    def get_cell_cost (O00000OO0000O00O0 ,OOOO0OO0O0O000OO0 ,O0000000OOO0OO0OO ):#line:32
+        if O0000000OOO0OO0OO [0 ]<0 or O0000000OOO0OO0OO [0 ]>O00000OO0000O00O0 .grid_size -1 or O0000000OOO0OO0OO [1 ]<0 or O0000000OOO0OO0OO [1 ]>O00000OO0000O00O0 .grid_size -1 :#line:34
+            return 1e9 #line:35
+        else :#line:36
+            OO0OO00O0OO0OO00O =OOOO0OO0O0O000OO0 [O0000000OOO0OO0OO [1 ],O0000000OOO0OO0OO [0 ]]#line:37
+            if OO0OO00O0OO0OO00O ==0 :#line:38
+                return 0 #line:39
+            else :#line:40
+                return [100 ,10 ,0 ][(O00000OO0000O00O0 .id -OO0OO00O0OO0OO00O )%3 ]#line:41
+    def get_new_target (O000OOOO0OO0O00OO ,O0O000OO000OO0O00 ):#line:43
+        O0000OOO00OO0O0OO =1e9 #line:44
+        for O000OOO00O00OOO0O in O000OOOO0OO0O00OO .cross :#line:46
+            O00OO0000OOOO0O0O =O000OOOO0OO0O00OO .get_cell (O000OOOO0OO0O00OO .position ,O000OOO00O00OOO0O )#line:48
+            OOO000OOOO0000O0O =O000OOOO0OO0O00OO .get_cell_cost (O0O000OO000OO0O00 ,O00OO0000OOOO0O0O )#line:50
+            if OOO000OOOO0000O0O <O0000OOO00OO0O0OO :#line:51
+                O0000OOO00OO0O0OO =OOO000OOOO0000O0O #line:52
+                O000OOOO0OO0O00OO .target =O00OO0000OOOO0O0O #line:53
+        if O0000OOO00OO0O0OO >0 :#line:55
+            O000OOOO0OO0O00OO .target [0 ]=random .randint (0 ,O0O000OO000OO0O00 .shape [0 ]-1 )#line:56
+            O000OOOO0OO0O00OO .target [1 ]=random .randint (0 ,O0O000OO000OO0O00 .shape [1 ]-1 )#line:57
+    def determine_next_move (O0O0O0000O0OOO00O ,OO00OOOO0OOOO0O0O ,O0OOO0OO0O00OO000 ,OO00OO0O0O0O0OOOO ):#line:59
+        if O0O0O0000O0OOO00O .grid_size ==None :#line:61
+            O0O0O0000O0OOO00O .grid_size =OO00OO0O0O0O0OOOO .grid_size #line:62
+        if O0O0O0000O0OOO00O .target is None :#line:65
+            O0O0O0000O0OOO00O .target =copy (O0O0O0000O0OOO00O .position )#line:66
+        if np .array_equal (O0O0O0000O0OOO00O .position ,O0O0O0000O0OOO00O .target ):#line:69
+            O0O0O0000O0OOO00O .get_new_target (OO00OOOO0OOOO0O0O )#line:70
+        if O0O0O0000O0OOO00O .target [0 ]>O0O0O0000O0OOO00O .position [0 ]:#line:73
+            return Move .RIGHT #line:74
+        elif O0O0O0000O0OOO00O .target [0 ]<O0O0O0000O0OOO00O .position [0 ]:#line:75
+            return Move .LEFT #line:76
+        elif O0O0O0000O0OOO00O .target [1 ]>O0O0O0000O0OOO00O .position [1 ]:#line:77
+            return Move .UP #line:78
+        else :#line:79
+            return Move .DOWN #line:80
